cb7ee152eac5c65191b32a7e5decb522d8c716bb
zolizoli <zoltan.varju@gmail.com>
2018-09-14 13:26:09 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Data sources and preprocessing
+##### Original shape file
+##### Level Admin8 and Admin9 layers were merged by using QGIS
+##### The merged layer was simplified by using QGIS
+##### Statistical data comes from Periféria
--- src/inspect_json.py
+++ src/inspect_json.py
@@ -1,14 +1,6 @@
 import json
 import pandas as pd
 ###############################################################################
-#####                 Data sources and preprocessing                      #####
-###############################################################################
-##### Original shape file 
-##### Level Admin8 and Admin9 layers were merged by using QGIS
-##### The merged layer was simplified by using QGIS
-##### Statistical data comes from Periféria
-
-###############################################################################
 #####                    Infor for the GeoJSON file                       #####
 ###############################################################################
 time_map = {"type": "FeatureCollection",
